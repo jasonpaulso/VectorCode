a76fd9a8b68e848a6e7183b38bdb891ed1a613ea
Davidyz <hzjlyz@gmail.com>
2025-03-23 20:20:28 +0000

    refactor(cli): refactor to make testing easier

--- src/vectorcode/mcp_main.py
+++ src/vectorcode/mcp_main.py
@@ -37,6 +37,72 @@ default_client: Optional[AsyncClientAPI] = None
 default_collection: Optional[AsyncCollection] = None
 
 
+async def list_collections() -> list[str]:
+    global default_config, default_client, default_collection
+    names: list[str] = []
+    client = default_client
+    if client is None:
+        # load from global config when failed to detect a project-local config.
+        client = await get_client(await load_config_file())
+    async for col in get_collections(client):
+        if col.metadata is not None:
+            names.append(str(col.metadata.get("path")))
+    return names
+
+
+async def query_tool(
+    n_query: int, query_messages: list[str], project_root: str
+) -> list[str]:
+    """
+    n_query: number of files to retrieve;
+    query_messages: keywords to query.
+    collection_path: Directory to the repository;
+    """
+    if not os.path.isdir(project_root):
+        raise McpError(
+            ErrorData(
+                code=1,
+                message="Use `list_collections` tool to get a list of valid paths for this field.",
+            )
+        )
+    else:
+        config = await get_project_config(project_root)
+        try:
+            client = await get_client(config)
+            collection = await get_collection(client, config, False)
+        except Exception:
+            raise McpError(
+                ErrorData(
+                    code=1,
+                    message=f"Failed to access the collection at {project_root}. Use `list_collections` tool to get a list of valid paths for this field.",
+                )
+            )
+    if collection is None:
+        raise McpError(
+            ErrorData(
+                code=1,
+                message=f"Failed to access the collection at {project_root}. Use `list_collections` tool to get a list of valid paths for this field.",
+            )
+        )
+    query_config = await config.merge_from(
+        Config(n_result=n_query, query=query_messages)
+    )
+    result_paths = await get_query_result_files(
+        collection=collection,
+        configs=query_config,
+    )
+    results: list[str] = []
+    for path in result_paths:
+        if os.path.isfile(path):
+            with open(path) as fin:
+                rel_path = os.path.relpath(path, config.project_root)
+                results.append(
+                    f"<path>{rel_path}</path>\n<content>{fin.read()}</content>",
+                )
+
+    return results
+
+
 async def mcp_server():
     global default_config, default_client, default_collection
     # sys.stderr = open(os.devnull, "w")
@@ -56,83 +122,29 @@ async def mcp_server():
         except InvalidCollectionException:
             default_collection = None
 
-    @mcp.tool(
-        "ls",
+    mcp.add_tool(
+        fn=list_collections,
+        name="ls",
         description="List all projects indexed by VectorCode. Call this before making queries.",
     )
-    async def list_collections() -> list[str]:
-        names: list[str] = []
-        client = default_client
-        if client is None:
-            # load from global config when failed to detect a project-local config.
-            client = await get_client(await load_config_file())
-        async for col in get_collections(client):
-            if col.metadata is not None:
-                names.append(str(col.metadata.get("path")))
-        return names
 
-    @mcp.tool(
-        "query",
-        description="Use VectorCode to perform vector similarity search on the repository and return a list of relevant file paths and contents. Make sure `project_root` is one of the values from the `list_collections` tool.",
+    mcp.add_tool(
+        fn=query_tool,
+        name="query",
+        description="Use VectorCode to perform vector similarity search on repositories and return a list of relevant file paths and contents. Make sure `project_root` is one of the values from the `list_collections` tool.",
     )
-    async def query_tool(
-        n_query: int, query_messages: list[str], project_root: str
-    ) -> list[str]:
-        """
-        n_query: number of files to retrieve;
-        query_messages: keywords to query.
-        collection_path: Directory to the repository;
-        """
-        if not os.path.isdir(project_root):
-            raise McpError(
-                ErrorData(
-                    code=1,
-                    message="Use `list_collections` tool to get a list of valid paths for this field.",
-                )
-            )
-        else:
-            config = await get_project_config(project_root)
-            try:
-                client = await get_client(config)
-                collection = await get_collection(client, config, False)
-            except Exception:
-                raise McpError(
-                    ErrorData(
-                        code=1,
-                        message=f"Failed to access the collection at {project_root}. Use `list_collections` tool to get a list of valid paths for this field.",
-                    )
-                )
-        if collection is None:
-            raise McpError(
-                ErrorData(
-                    code=1,
-                    message=f"Failed to access the collection at {project_root}. Use `list_collections` tool to get a list of valid paths for this field.",
-                )
-            )
-        query_config = await config.merge_from(
-            Config(n_result=n_query, query=query_messages)
-        )
-        result_paths = await get_query_result_files(
-            collection=collection,
-            configs=query_config,
-        )
-        results: list[str] = []
-        for path in result_paths:
-            if os.path.isfile(path):
-                with open(path) as fin:
-                    rel_path = os.path.relpath(path, config.project_root)
-                    results.append(
-                        f"<path>{rel_path}</path>\n<content>{fin.read()}</content>",
-                    )
 
-        return results
+    return mcp
 
+
+async def run_server():
+    mcp = await mcp_server()
     await mcp.run_stdio_async()
     return 0
 
 
 def main():
-    return asyncio.run(mcp_server())
+    return asyncio.run(run_server())
 
 
 if __name__ == "__main__":
